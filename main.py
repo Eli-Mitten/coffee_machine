f9168cdb18d2b488169022836c75888d53d2721f
Eli-Mitten <elisabeth.mitten@gmail.com>
2022-12-22 17:35:02 +0100

    End of project

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from db import MENU, resources, money
+from db import MENU, resources
 
 coffee_machine_is = True
+money = 0
+
+
+def show_report():
+    global money
+    print("__________REPORT__________")
+    for k, v in resources.items():
+        print(f"    {k}:{v}")
+    print(f"    money: ${money}")
+    print("__________END__________")
 
 
 def check_resources(_selection_):
@@ -8,42 +18,72 @@ def check_resources(_selection_):
     i = MENU[_selection_]["ingredients"]
     for key, value in i.items():
         if value >= r[key]:
-            print(f"Sorry there is not enough {key}, left {value}")
+            print(f"Sorry there is not enough {key}, left {r[key]}, you need {value}")
             return False
     return True
 
 
-def give_me_de_money():
-    total = 0
-    # coger el dinero del usuario
-    money = input("Insert")
-    # calcular cuanto es en total
+def give_me_the_money():
+    
+    total = int(input("Who many quarters?: ")) * 0.25
+    total += int(input("Who many dimes?: ")) * 0.10
+    total += int(input("Who many nickel: ")) * 0.05
+    total += int(input("Who many pennies?: ")) * 0.01
+    print(f"Total inserted: $ {round(total, 2)}")
+    return round(total, 2)
 
-    return total
+
+def is_money_enough(amount, selected_product):
+    if not amount >= MENU[selected_product]['cost']:
+        print(f"Importe insuficinete, return {amount}")
+        return False
+    else:
+        print("Importe correcto")
+        return True
+
+
+def save_the_money(_amount, product_price):
+    global money
+    money += product_price
+    print(f"Return: iserted money {_amount} - product price {product_price} = ${round(_amount - product_price)}")
+    show_report()
+
+
+def delete_product_from_resources(selected_product):
+    products_ingredients = MENU[selected_product]["ingredients"]
+
+    for key, value in products_ingredients.items():
+        resources[key] -= value
 
 
 def make_coffee(_selection):
+    global money
+    product_cost = MENU[_selection]['cost']
+
     # Check if there are enough resources
     if not check_resources(_selection):
         return
-    else:
-        print("Recursos suficientes en la maquina")
-    # Show the selection price
-    print(f"You selected {_selection}, please inset ${MENU[_selection]['cost']}")
-    # Give me the f**ing money
-    give_me_de_money()
 
-    print("Todo ok")
+    # Show the selection price
+    print(f"You selected {_selection}, please inset ${product_cost}")
+
+    # Give me the f**ing money
+    total_amount = give_me_the_money()
+    if not is_money_enough(total_amount, _selection):
+        return
+    delete_product_from_resources(_selection)
+    save_the_money(total_amount, product_cost)
+    print(f"Enjoy your {_selection}!!")
 
 
 while coffee_machine_is:
-    selection = input("What would you like? (espresso/latte/cappuccino):")
+    selection = input("What would you like? (espresso/latte/cappuccino) admin: (report/off):")
     if selection == 'report':
-        for k, v in resources.items():
-            print(f"{k}: {v}")
-        print(f"money: ${money}")
+        show_report()
     elif selection == 'espresso' or selection == 'latte' or selection == 'cappuccino':
         make_coffee(selection)
     elif selection == 'off':
         coffee_machine_is = False
         print("Switch off")
+    else:
+        print("Wrong selection")
